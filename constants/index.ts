ac7c4a76430af4b47d448a8d0913b8a578e28290
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2024-06-05 20:59:34 -0500

    cleanup typos

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+    { href: '/', key: 'how_hilink_works', label: 'How Hilink Works?' },
     { href: '/', key: 'services', label: 'Services' },
     { href: '/', key: 'pricing ', label: 'Pricing ' },
     { href: '/', key: 'contact_us', label: 'Contact Us' },
@@ -32,7 +32,7 @@ export const NAV_LINKS = [
         "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
     },
     {
-      title: 'Technology using augment reality',
+      title: 'Technology using augmented reality',
       icon: '/tech.svg',
       variant: 'green',
       description:
